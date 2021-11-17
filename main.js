5b45234432a3e9fa76f63da5c77b07a842ef4b5a
Mattis Kleine <58635065+mattiskleine@users.noreply.github.com>
2021-11-17 21:09:31 +0100

    first upload of local files

---
+++ main.js
@@ -0,0 +1,16 @@
+//cd Downloads/Webpage_Mattis_Kleine
+//nodemon
+
+const express = require("express");
+const app = express();
+const port = 2222;
+
+app.listen(port, () => {
+  console.log("Application started and Listening on port " + port);
+});
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
